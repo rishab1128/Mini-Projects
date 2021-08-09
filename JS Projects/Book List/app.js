3333a3ffaeb4ebb378ab1d4c730331e83cdb1b88
rishab1128 <rishabpurkayastha28@gmail.com>
2021-08-10 03:39:25 +0530

    ES5 app.js discarded

--- JS Projects/Book List/app.js
+++ JS Projects/Book List/app.js
@@ -1,73 +1,133 @@
-//Book Constructor
-function Book(title, author , isbn){
-    this.title=title;
-    this.author=author;
-    this.isbn=isbn;
-}
-
-
-
-//UI Constructor
-function UI(){
-
-}
-
-//Add Book to list
-UI.prototype.addBooktoList=function(book){
-    const list=document.getElementById('book-list');
-
-    //Create a tr element
-    const row=document.createElement('tr');
-
-    //Insert cols
-    row.innerHTML=` 
-        <td>${book.title}</td>
-        <td>${book.author}</td>
-        <td>${book.isbn}</td>
-        <td> <a href="#" class="delete" > X </a> </td>
-    `
-
-    //row.className=`${book.isbn}`;
-    console.log(row);
-
-    //Append row to book-list
-    list.appendChild(row);
-    
-}
-
-//Delete book-row from the list
-UI.prototype.deleteBook=function(target){
-    if(target.className==='delete')
-    {
-        target.parentElement.parentElement.remove();
+class Book{
+    constructor(title,author,isbn){
+        this.title=title;
+        this.author=author;
+        this.isbn=isbn;
     }
 }
 
-//Clear input fields (after we click submit)
-UI.prototype.clearFields=function(){
-    document.getElementById('title').value='';
-    document.getElementById('author').value='';
-    document.getElementById('isbn').value='';
+class UI{
+
+    addBooktoList(book){
+        const list=document.getElementById('book-list');
+
+        //Create a tr element
+        const row=document.createElement('tr');
+
+        //Insert cols
+        row.innerHTML=` 
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td id="${book.isbn}">${book.isbn}</td>
+            <td> <a href="#" class="delete" > X </a> </td>
+        `
+
+        //row.className=`${book.isbn}`;
+        //console.log(row);
+
+        //Append row to book-list
+        list.appendChild(row);
+    }
+
+    deleteBook(target){
+        if(target.className==='delete')
+        {
+            target.parentElement.parentElement.remove();
+        }
+
+    }
+
+    clearFields(){
+        document.getElementById('title').value='';
+        document.getElementById('author').value='';
+        document.getElementById('isbn').value='';
+    }
+
+    showAlert(msg,className){
+        //Create a div
+        const div=document.createElement('div');
+        div.className=`alert ${className}`; 
+        const text=document.createTextNode(msg);
+        div.appendChild(text);
+
+        //Add the div to the DOM -->specifically before the form
+        const container=document.querySelector('.container');
+        const form=document.getElementById('book-form');
+        container.insertBefore(div,form);
+
+        //Disappear the error message after 3s
+        setTimeout(function(){
+            document.querySelector(`.alert`).remove();
+        } , 3000);
+    }
+
 }
 
-//Show Alert
-UI.prototype.showAlert=function(msg, className){
-    //Create a div
-    const div=document.createElement('div');
-    div.className=`alert ${className}`; 
-    const text=document.createTextNode(msg);
-    div.appendChild(text);
+//Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        //local Storage stores values in the form of key-value pairs 
+        //Here we are looking for the key books if it is present or not in the local Storage
+        if(localStorage.getItem('books')===null){
+            books=[];
+        }
+        else{
+            books=JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
 
-    //Add the div to the DOM -->specifically before the form
-    const container=document.querySelector('.container');
-    const form=document.getElementById('book-form');
-    container.insertBefore(div,form);
+    static displayBooks(){
+        const books=Store.getBooks();
+        books.forEach(function(book){
+            const ui=new UI();
+            //Add book to ui
+            ui.addBooktoList(book);
+        })
+    }
 
-    //Disappear the error message after 3s
-    setTimeout(function(){
-        document.querySelector(`.alert`).remove();
-    } , 3000);
+    static addBook(book){
+        const books=Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+
+    //Method 1 , of removing book from local storage
+    static removeBook(book_row){
+        //console.log(book_row.children[2].id);
+        const delete_book_id=book_row.children[2].id;
+        const books=Store.getBooks();
+        books.forEach(function(book){
+            if(book.isbn==delete_book_id)
+            {
+                console.log(book.isbn , books.indexOf(book));
+                const idx=books.indexOf(book);
+                books.splice(idx,1);
+                return;
+            }
+        })
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+
+    //Method 2 , of removing book from local storage
+    static removeBook2(isbn){
+
+        const books=Store.getBooks();
+        books.forEach(function(book,index){
+            if(book.isbn==isbn)
+            {
+                books.splice(index,1);
+                return;
+            }
+        })
+        localStorage.setItem('books',JSON.stringify(books));
+    }
 }
+
+
+//DOM Load Event
+document.addEventListener('DOMContentLoaded',Store.displayBooks)
 
 
 //Event Listener for submit (for adding a book)
@@ -86,6 +146,7 @@ function runSubmit(e){
 
     //Instantiate UI
     const ui=new UI();
+    console.log(ui);
 
     
     if(title==='' || author==='' || isbn==='')
@@ -97,6 +158,9 @@ function runSubmit(e){
     {
         //Add book to list
         ui.addBooktoList(book);
+
+        //Add book to local storage
+        Store.addBook(book);
 
         //Clear input fields
         ui.clearFields();
@@ -114,6 +178,12 @@ document.getElementById('book-list').addEventListener('click',function(e){
     //Delete book
     const ui=new UI();
     ui.deleteBook(e.target);
+
+    //Delete book from local storage using Method 1 removeBook
+    /*Store.removeBook(e.target.parentElement.parentElement);*/
+
+    //Delete book from local storage using Method 2 removeBook2
+    Store.removeBook2(e.target.parentElement.previousElementSibling.textContent);
 
     //Show alert
     ui.showAlert('Book deleted!!','success');
